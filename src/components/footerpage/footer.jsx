9de8941a43e6ee06cd8a9faa723526a514a1b2d7
Gowtham1802 <gowthama7575@gmail.com>
2024-01-19 14:21:26 +0530

    add

--- src/components/footerpage/footer.jsx
+++ src/components/footerpage/footer.jsx
@@ -12,144 +12,158 @@ import { IoIosArrowForward } from "react-icons/io";
 const FooterPage = () => {
   return (
     <div>
-      <div className="w-full bg-black flex flex-col justify-start">
-        <div className="w-full flex flex-row justify-start sm:flex-col sm:justify-center lg:w-full lg:flex-row lg:justify-start ">
-          <div className="w-[28%] flex flex-col gap-8 px-[5rem] pt-[5rem] pb-[0rem] items-center sm:w-[100%]">
-            <div className="flex flex-row gap-2 items-center sm:items-center">
+      <div className="w-full bg-black flex flex-col justify-start overflow-x-hidden">
+        <div className="w-full flex flex-col justify-start items-center sm:flex-col sm:justify-center lg:w-full lg:flex-row lg:justify-start ">
+          <div className="w-[28%] flex flex-col gap-8 px-[5rem] pt-[3rem] pb-[1rem] items-center sm:w-[100%] lg:w-[28%] lg:px-[2rem] lg:gap-5 ">
+            <div className="flex flex-row gap-2 items-center sm:items-center lg:gap-1 ">
               <img
-                className="w-20 h-20 rounded-full"
+                className="w-20 h-20 rounded-full lg:w-14 lg:h-14 "
                 src={FooterLogo}
                 alt="footer-logo"
               />
-              <h1 className="text-[white] flex flex-col items-center">
-                <span className="text-[#ff9637] text-[40px] border-b-[3px] border-b-orange-400 ">
+              <h1 className="text-[white] flex flex-col items-center lg:items-start ">
+                <span className="text-[#ff9637] text-[40px] border-b-[3px] border-b-orange-400 lg:text-[25px]  ">
                   SUGUNA
                 </span>
                 {/* <br />{" "} */}
-                <span className=" text-[#83acb0] font-medium">
+                <span className=" text-[#83acb0] font-medium lg:text-[10px] ">
                   POLYTECHNIC COLLEGE
                 </span>
               </h1>
             </div>
-            <div className="flex gap-6 sm:flex-row sm:items-center">
+            <div className="flex gap-6 sm:flex-row sm:items-center lg:gap-4 ">
               <div className="">
-                <FaFacebookF className="bg-white hover:bg-[#ff9637] hover:text-white cursor-pointer w-12 h-12 p-3 text-[#ff9637] rounded-full hover:scale-[0.9] transition-all delay-100"></FaFacebookF>
+                <FaFacebookF className="bg-white hover:bg-[#ff9637] hover:text-white cursor-pointer w-12 h-12 p-3 text-[#ff9637] rounded-full hover:scale-[0.9] transition-all delay-100 lg:w-[35px] lg:h-[35px] lg:p-[6px]  "></FaFacebookF>
               </div>
               <div className="">
-                <FaTwitter className="bg-white hover:bg-[#ff9637] hover:text-white cursor-pointer w-12 h-12 p-3 text-[#ff9637] rounded-full hover:scale-[0.9] transition-all delay-100"></FaTwitter>
+                <FaTwitter className="bg-white hover:bg-[#ff9637] hover:text-white cursor-pointer w-12 h-12 p-3 text-[#ff9637] rounded-full hover:scale-[0.9] transition-all delay-100 lg:w-[35px] lg:h-[35px] lg:p-[6px] "></FaTwitter>
               </div>
               <div className="">
-                <FaInstagramSquare className="bg-white hover:bg-[#ff9637] hover:text-white cursor-pointer w-12 h-12 p-3 text-[#ff9637] rounded-full hover:scale-[0.9] transition-all delay-100"></FaInstagramSquare>
+                <FaInstagramSquare className="bg-white hover:bg-[#ff9637] hover:text-white cursor-pointer w-12 h-12 p-3 text-[#ff9637] rounded-full hover:scale-[0.9] transition-all delay-100 lg:w-[35px] lg:h-[35px] lg:p-[6px] "></FaInstagramSquare>
               </div>
               <div className="">
-                <FaYoutube className="bg-white hover:bg-[#ff9637] hover:text-white cursor-pointer w-12 h-12 p-3 text-[#ff9637] rounded-full hover:scale-[0.9] transition-all delay-100"></FaYoutube>
+                <FaYoutube className="bg-white hover:bg-[#ff9637] hover:text-white cursor-pointer w-12 h-12 p-3 text-[#ff9637] rounded-full hover:scale-[0.9] transition-all delay-100 lg:w-[35px] lg:h-[35px] lg:p-[6px] "></FaYoutube>
               </div>
             </div>
           </div>
-          <div className="w-[24%] p-[5rem] pt-[4rem] pb-[3rem] flex sm:w-[100%] sm-flex sm:flex-col sm:items-center sm:text-center sm:justify-center">
-            <h1 className="text-white text-[2rem] font-bold pb-2">
-              Quick Links
-            </h1>
-            <ul className="text-white text-[20px] sm:flex sm:flex-col sm:justify-center sm: sm:text-center ">
-              <li className=" hover:text-[#ff9637] cursor-pointer pb-2 sm:text-center">
-                <a
-                  className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
-                  href=""
-                >
-                  <IoIosArrowForward></IoIosArrowForward>About Us
-                </a>
-              </li>
-              <li className="hover:text-[#ff9637] cursor-pointer pb-2">
-                <a
-                  className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
-                  href=""
-                >
-                  <IoIosArrowForward></IoIosArrowForward>Awards
-                </a>
-              </li>
-              <li className="hover:text-[#ff9637] cursor-pointer pb-2">
-                <a
-                  className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
-                  href=""
-                >
-                  <IoIosArrowForward></IoIosArrowForward>Facilities
-                </a>
-              </li>
-              <li className="hover:text-[#ff9637] cursor-pointer pb-2">
-                <a
-                  className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
-                  href=""
-                >
-                  <IoIosArrowForward></IoIosArrowForward>Departments
-                </a>
-              </li>
-              <li className="hover:text-[#ff9637] cursor-pointer pb-2">
-                <a
-                  className="flex justify-start items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
-                  href=""
-                >
-                  <IoIosArrowForward></IoIosArrowForward>Anti-Ragging Committee
-                </a>
-              </li>
-            </ul>
+          <div className="w-[24%] p-[5rem] pt-[4rem] pb-[3rem] flex sm:w-[100%] sm-flex sm:flex-col sm:items-center sm:text-center sm:justify-center lg:w-[48%] lg:p-[2rem] lg:pt-[2rem] lg:pb[1rem]lg:flex lg:flex-row">
+            <div className="lg:w-[50%]">
+              <h1 className="text-white text-[2rem] font-bold pb-2 lg:text-[1.5rem] text-left">
+                Quick Links
+              </h1>
+              <ul className="text-white text-[20px] sm:flex sm:flex-col sm:justify-center sm:text-center lg:text-[18px] w-full">
+                <li className=" hover:text-[#ff9637] cursor-pointer pb-2 sm:text-center">
+                  <a
+                    className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
+                    href=""
+                  >
+                    <IoIosArrowForward className="text-[20px]"></IoIosArrowForward>
+                    About Us
+                  </a>
+                </li>
+                <li className="hover:text-[#ff9637] cursor-pointer pb-2">
+                  <a
+                    className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
+                    href=""
+                  >
+                    <IoIosArrowForward className="text-[20px]"></IoIosArrowForward>
+                    Awards
+                  </a>
+                </li>
+                <li className="hover:text-[#ff9637] cursor-pointer pb-2">
+                  <a
+                    className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
+                    href=""
+                  >
+                    <IoIosArrowForward className="text-[20px]"></IoIosArrowForward>
+                    Facilities
+                  </a>
+                </li>
+                <li className="hover:text-[#ff9637] cursor-pointer pb-2">
+                  <a
+                    className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
+                    href=""
+                  >
+                    <IoIosArrowForward className="text-[20px]"></IoIosArrowForward>
+                    Departments
+                  </a>
+                </li>
+                <li className="hover:text-[#ff9637] cursor-pointer pb-2">
+                  <a
+                    className="flex justify-start gap-2 items-center hover:scale-[0.9] transition-all duration-[600ms]"
+                    href=""
+                  >
+                    <IoIosArrowForward className="text-[20px]"></IoIosArrowForward>
+                    <p className="text-left">Anti-Ragging Committee</p>
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="w-[24%] pt-[rem] pb-[3rem] flex sm:w-[100%] sm:flex-col sm:items-start lg:pl-[7.2rem] lg:pt-[3rem] lg:pb[1rem] lg:flex-col lg:justify-start lg:items-start lg:w-[50%] ">
+              <ul className="text-white text-[20px] lg:text-[18px]">
+                <li className="hover:text-[#ff9637] cursor-pointer pb-2">
+                  <a
+                    className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
+                    href=""
+                  >
+                    <IoIosArrowForward className="text-[20px]"></IoIosArrowForward>
+                    Admission
+                  </a>
+                </li>
+                <li className="hover:text-[#ff9637] cursor-pointer pb-2">
+                  {" "}
+                  <a
+                    className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
+                    href=""
+                  >
+                    <IoIosArrowForward className="text-[20px]"></IoIosArrowForward>
+                    Scholarship
+                  </a>
+                </li>
+                <li className="hover:text-[#ff9637] cursor-pointer pb-2">
+                  <a
+                    className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
+                    href=""
+                  >
+                    <IoIosArrowForward className="text-[20px]"></IoIosArrowForward>
+                    Our Recruiters
+                  </a>
+                </li>
+                <li className="hover:text-[#ff9637] cursor-pointer pb-2">
+                  <a
+                    className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
+                    href=""
+                  >
+                    <IoIosArrowForward className="text-[20px]"></IoIosArrowForward>
+                    Placements
+                  </a>
+                </li>
+                <li className="hover:text-[#ff9637] cursor-pointer pb-2">
+                  <a
+                    className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
+                    href=""
+                  >
+                    <IoIosArrowForward className="text-[20px]"></IoIosArrowForward>
+                    Photo Gallery
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className="w-[24%] p-[5.5rem] pt-[7rem] pb-[3rem] flex sm:w-[100%] sm:flex-col sm:items-center ">
-            <ul className="text-white text-[20px] ">
-              <li className="hover:text-[#ff9637] cursor-pointer pb-2">
-                <a
-                  className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
-                  href=""
-                >
-                  <IoIosArrowForward></IoIosArrowForward>Admission
-                </a>
-              </li>
-              <li className="hover:text-[#ff9637] cursor-pointer pb-2">
-                {" "}
-                <a
-                  className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
-                  href=""
-                >
-                  <IoIosArrowForward></IoIosArrowForward>Scholarship
-                </a>
-              </li>
-              <li className="hover:text-[#ff9637] cursor-pointer pb-2">
-                <a
-                  className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
-                  href=""
-                >
-                  <IoIosArrowForward></IoIosArrowForward>Our Recruiters
-                </a>
-              </li>
-              <li className="hover:text-[#ff9637] cursor-pointer pb-2">
-                <a
-                  className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
-                  href=""
-                >
-                  <IoIosArrowForward></IoIosArrowForward>Placements
-                </a>
-              </li>
-              <li className="hover:text-[#ff9637] cursor-pointer pb-2">
-                <a
-                  className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
-                  href=""
-                >
-                  <IoIosArrowForward></IoIosArrowForward>Photo Gallery
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="w-[25%] p-[3rem] pt-[4rem] pb-[3rem] sm:w-[100%] sm:flex sm:justify-center sm:items-center sm:flex-col">
-            <h1 className="text-[white] text-[2rem] font-bold pb-2">
+
+          <div className="w-[25%] p-[3rem] pt-[4rem] pb-[3rem] sm:w-[100%] sm:flex sm:justify-start sm:items-center sm:flex-col sm:pt-[0rem] lg:w-[24%] lg:p-[2rem] lg:pt-[3rem] lg:pb[1rem]">
+            <h1 className="text-[white] text-[2rem] font-bold pb-2 sm:justify-start sm:items-start lg:text-[1.5rem]  ">
               Our Institutions
             </h1>
-            <ul className="text-[white] text-[20px]">
+            <ul className="text-[white] text-[20px] sm:justify-start sm:items-start sm:pl-[6.3rem] lg:text-[18px]">
               <li className="hover:text-[#ff9637] cursor-pointer pb-2">
                 <a
                   className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
                   href=""
                 >
-                  <IoIosArrowForward></IoIosArrowForward>Suguna College of Arts
-                  and Science
+                  <IoIosArrowForward className="text-[20px]"></IoIosArrowForward>
+                  Suguna College of Arts and Science
                 </a>
               </li>
               <li className="hover:text-[#ff9637] cursor-pointer pb-2">
@@ -157,8 +171,8 @@ const FooterPage = () => {
                   className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
                   href=""
                 >
-                  <IoIosArrowForward></IoIosArrowForward>Suguna College of
-                  Engineering
+                  <IoIosArrowForward className="text-[20px]"></IoIosArrowForward>
+                  Suguna College of Engineering
                 </a>
               </li>
               <li className="hover:text-[#ff9637] cursor-pointer pb-2">
@@ -166,7 +180,8 @@ const FooterPage = () => {
                   className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
                   href=""
                 >
-                  <IoIosArrowForward></IoIosArrowForward>Suguna PIP School
+                  <IoIosArrowForward className="text-[20px]"></IoIosArrowForward>
+                  Suguna PIP School
                 </a>
               </li>
               <li className="hover:text-[#ff9637] cursor-pointer pb-2">
@@ -174,49 +189,53 @@ const FooterPage = () => {
                   className="flex items-center gap-2 hover:scale-[0.9] transition-all duration-[600ms]"
                   href=""
                 >
-                  <IoIosArrowForward></IoIosArrowForward>Suguna Innovation
-                  Institute
+                  <IoIosArrowForward className="text-[20px]"></IoIosArrowForward>
+                  Suguna Innovation Institute
                 </a>
               </li>
             </ul>
           </div>
         </div>
-        <div className="w-full flex flex-row justify-evenly items-center pb-[7rem] sm:flex-col sm:gap-4">
-          <div className="flex justify-around items-center gap-2  ">
-            <IoCall className="text-[#ff9637] text-[3rem]"></IoCall>
+        <div className="w-full flex flex-row justify-evenly items-center pb-[7rem] sm:flex-col sm:gap-4 lg:flex lg:flex-row lg:justify-evenly lg:items-center lg:pt-[2rem] lg:pb-[3.3rem] ">
+          <div className="flex justify-around items-center gap-2 sm:flex-col lg:flex-row ">
+            <IoCall className="text-[#ff9637] text-[3rem] lg:text-[2rem] "></IoCall>
             <div>
-              <span className="text-[white] text-[20px]">+91 8870008623</span>
+              <span className="text-[white] text-[20px] lg:text-[14px] ">
+                +91 8870008623
+              </span>
               <br />
-              <span className="text-[white] text-[20px]">+91 9600860934</span>
+              <span className="text-[white] text-[20px] lg:text-[14px] ">
+                +91 9600860934
+              </span>
             </div>
           </div>
-          <div className=" flex items-center gap-2 ">
-            <div className="flex items-center gap-2">
-              <MdEmail className="text-[#ff9637] text-[3rem] "></MdEmail>
+          <div className=" flex items-center gap-2 sm:flex sm:flex-col ">
+            <div className="flex items-center gap-2 sm:flex-col lg:flex-row">
+              <MdEmail className="text-[#ff9637] text-[3rem] lg:text-[2rem] "></MdEmail>
               <div>
-                <span className="text-[white] text-[20px] ">
+                <span className="text-[white] text-[20px] lg:text-[14px] ">
                   sugunapolytechniccollege@gmail.com
                 </span>
               </div>
             </div>
           </div>
-          <div className="flex  items-center gap-2">
+          <div className="flex  items-center gap-2 sm:flex-col lg:flex-row">
             <a href="https://www.google.com/maps/place/Suguna+Polytechnic+College/@11.0509439,77.0322591,15z/data=!4m6!3m5!1s0x3ba857bbffffffff:0xfaed96bd33545bc0!8m2!3d11.0509439!4d77.0322591!16s%2Fg%2F1tl7m4b8?entry=ttu">
-              <FaLocationDot className="text-[#ff9637] text-[3rem]"></FaLocationDot>
+              <FaLocationDot className="text-[#ff9637] text-[3rem] lg:text-[2rem] "></FaLocationDot>
             </a>
             <div className="items-center">
-              <span className="text-[white] font-bold text-[20px]">
+              <span className="text-[white] font-bold text-[20px] lg:text-[14px] ">
                 Kalapatti Main Rd, near Vinayaka Temple, GKD Nagar,
               </span>
               <br />
-              <span className="text-[white] text-[20px]">
+              <span className="text-[white] text-[20px] lg:text-[14px] ">
                 Nehru Nagar West, Coimbatore, Tamil Nadu - 641014
               </span>
             </div>
           </div>
         </div>
-        <div className="w-full items-center text-center pt-[rem] pb-[3rem]">
-          <span className="text-[#979696] text-center items-center text-[20px]">
+        <div className="w-full items-center text-center pt-[rem] pb-[2rem]">
+          <span className="text-[#979696] text-center items-center text-[20px] lg:text-[14px] ">
             Copyright © 2023 Suguna Polytechnic College
           </span>
         </div>
