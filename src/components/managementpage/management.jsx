0952074b20f7619a77608a0b76946ea5526a4c9c
Gowtham1802 <gowthama7575@gmail.com>
2024-01-31 13:39:38 +0530

    add update page

--- src/components/managementpage/management.jsx
+++ src/components/managementpage/management.jsx
@@ -1,15 +1,217 @@
 // import SwipeableTextMobileStepper from "./managementcarousel";
+import Founder from "../../../public/images/founder-img/Founder1.png";
+import Chairman from "../../../public/images/chairman-img/Chairman1.png";
+import Trustee from "../../../public/images/trustee-img/Trustee1.png";
+import Director from "../../../public/images/director-img/Director1.png";
+import Principal from "../../../public/images/principal-img/Principal1.png";
+import Coordinator from "../../../public/images/coordinator-img/Coordinator1.png";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
 
 const ManagementPage = () => {
   return (
     <div>
-      <div className="">
-        <div className="">
-          <h1>Gowtham</h1>
+      <div className="w-[100%] mb-4">
+        <div className="text-center mt-6">
+          <span className="mb-3 flex items-center justify-center text-lg font-semibold text-[#1DC1D1]">
+            INSTRUCTORS
+          </span>
+          <h1 className="flex items-center justify-center text-3xl font-bold">
+            Expert Instructors
+          </h1>
         </div>
-      </div>
-      <div>
-        {/* <SwipeableTextMobileStepper></SwipeableTextMobileStepper> */}
+        <div className="w-full  justify-center items-center ">
+          <div className="w-full flex-row items-center  mt-6 justify-evenly grid grid-cols-3 gap-10 pl-[8rem]">
+            <div className="  h-[21rem] w-[20rem] bg-[#f4e5d8] rounded-lg">
+              <div className="overflow-hidden">
+                <img
+                  className="w-full h-[15.5rem]  object-cover hover:scale-[1.1] transition duration-[400ms]"
+                  src={Founder}
+                  alt="one"
+                />
+              </div>
+              <div className="relative mt-2">
+                <div className="absolute top-[-1.5rem] left-0 right-0 flex justify-center">
+                  <div className="flex items-center w-[8.5rem] h-[3rem] justify-center space-x-3 bg-[#f4e5d8]">
+                    <button className="bg-[#f98d2f] text-white p-2">
+                      <FaFacebookF />
+                    </button>
+                    <button className="bg-[#f98d2f] text-white p-2">
+                      <FaTwitter />
+                    </button>
+                    <button className="bg-[#f98d2f] text-white p-2">
+                      <FaLinkedin />
+                    </button>
+                  </div>
+                </div>
+                <div className="w-full h-[5rem] flex flex-col text-center justify-center bg-[#f98d2f] mt-0">
+                  <h5 className="mb-0 font-bold text-xl mt-2">
+                    Mr.G.Ramaswamy
+                  </h5>
+                  <small className="font-bold text-sm">Founder</small>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col h-[21rem] w-[20rem] bg-[#f4e5d8] rounded-lg">
+              <div className="overflow-hidden">
+                <img
+                  className="h-[20rem] w-[20rem] scale-[1] hover:scale-[1.1] transition duration-[400ms]"
+                  src={Chairman}
+                  alt="one"
+                />
+              </div>
+              <div className=" relative">
+                <div className="w-full  absolute top-[-1.5rem] left-0 right-0 flex justify-center">
+                  <div className="flex items-center mt-1 w-[8.5rem] h-[3rem]  justify-center space-x-3 bg-[#f4e5d8] ">
+                    <button className="bg-[#f98d2f] text-white p-2 ">
+                      <FaFacebookF />
+                    </button>
+                    <button className="bg-[#f98d2f] text-white p-2 ">
+                      <FaTwitter />
+                    </button>
+                    <button className="bg-[#f98d2f] text-white p-2 ">
+                      <FaLinkedin />
+                    </button>
+                  </div>
+                </div>
+                <div className="w-[20rem] h-[5rem] flex flex-col text-center justify-center bg-[#f98d2f] mt-0">
+                  <h5 className="mb-0 font-bold text-xl mt-6">
+                    Mr.V.Lakshiminarayanasamy
+                  </h5>
+                  <small className="font-bold text-sm">
+                    Managing Director & Chairman
+                  </small>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col h-[21rem] w-[20rem] bg-[#f4e5d8] rounded-lg">
+              <div className="overflow-hidden">
+                <img
+                  className="h-[20rem] w-[20rem] hover:scale-[1.3] scale-[1.1] transition duration-[400ms]"
+                  src={Trustee}
+                  alt="one"
+                />
+              </div>
+              <div className=" relative">
+                <div className="w-full  absolute top-[-1.5rem] left-0 right-0 flex justify-center">
+                  <div className="flex items-center mt-1 w-[8.5rem] h-[3rem]  justify-center space-x-3 bg-[#f4e5d8]">
+                    <button className="bg-[#f98d2f] text-white p-2 ">
+                      <FaFacebookF />
+                    </button>
+                    <button className="bg-[#f98d2f] text-white p-2 ">
+                      <FaTwitter />
+                    </button>
+                    <button className="bg-[#f98d2f] text-white p-2 ">
+                      <FaLinkedin />
+                    </button>
+                  </div>
+                </div>
+                <div className="w-[20rem] h-[5rem] flex flex-col text-center  justify-center bg-[#f98d2f] mt-0">
+                  <h5 className="mb-0 font-bold text-xl mt-6">Mrs.L.Suguna</h5>
+                  <small className="font-bold text-sm">Managing Trustee</small>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col  h-[21rem] w-[20rem] bg-[#f4e5d8] rounded-lg">
+              <div className="overflow-hidden">
+                <img
+                  className="h-[20rem] w-[20rem] scale-[1.2] hover:scale-[1.3]  transition duration-[400ms]"
+                  src={Director}
+                  alt="one"
+                />
+              </div>
+              <div className=" relative">
+                <div className="w-full  absolute top-[-1.5rem] left-0 right-0 flex justify-center">
+                  <div className="flex items-center mt-1 w-[8.5rem] h-[3rem]  justify-center space-x-3 bg-[#F0FBFC]">
+                    <button className="bg-[#f98d2f] text-white p-2 ">
+                      <FaFacebookF />
+                    </button>
+                    <button className="bg-[#f98d2f] text-white p-2 ">
+                      <FaTwitter />
+                    </button>
+                    <button className="bg-[#f98d2f] text-white p-2 ">
+                      <FaLinkedin />
+                    </button>
+                  </div>
+                </div>
+                <div className="w-[20rem] h-[5rem] flex flex-col text-center  justify-center bg-[#f98d2f] mt-0">
+                  <h5 className="mb-0 font-bold text-xl mt-6">
+                    {" "}
+                    Mr.Arun Ranganathan
+                  </h5>
+                  <small className="font-bold text-sm">Director</small>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col  h-[21rem] w-[20rem] bg-[#f4e5d8] rounded-lg">
+              <div className="overflow-hidden">
+                <img
+                  className="h-[20rem] w-[20rem] scale-[1.2] hover:scale-[1.3]  transition duration-[400ms]"
+                  src={Principal}
+                  alt="one"
+                />
+              </div>
+              <div className=" relative">
+                <div className="w-full  absolute top-[-1.5rem] left-0 right-0 flex justify-center">
+                  <div className="flex items-center mt-1 w-[8.5rem] h-[3rem]  justify-center space-x-3 bg-[#F0FBFC]">
+                    <button className="bg-[#f98d2f] text-white p-2 ">
+                      <FaFacebookF />
+                    </button>
+                    <button className="bg-[#f98d2f] text-white p-2 ">
+                      <FaTwitter />
+                    </button>
+                    <button className="bg-[#f98d2f] text-white p-2 ">
+                      <FaLinkedin />
+                    </button>
+                  </div>
+                </div>
+                <div className="w-[20rem] h-[5rem] flex flex-col text-center  justify-center bg-[#f98d2f] mt-0">
+                  <h5 className="mb-0 font-bold text-xl mt-6">
+                    {" "}
+                    Mr.V.Govindarajalu
+                  </h5>
+                  <small className="font-bold text-sm">Principlal</small>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col  h-[21rem] w-[20rem] bg-[#f4e5d8] rounded-lg">
+              <div className="overflow-hidden">
+                <img
+                  className="h-[20rem] w-[20rem] scale-[1.2] hover:scale-[1.3]  transition duration-[400ms]"
+                  src={Coordinator}
+                  alt="one"
+                />
+              </div>
+              <div className=" relative">
+                <div className="w-full  absolute top-[-1.5rem] left-0 right-0 flex justify-center">
+                  <div className="flex items-center mt-1 w-[8.5rem] h-[3rem]  justify-center space-x-3 bg-[#F0FBFC]">
+                    <button className="bg-[#f98d2f] text-white p-2 ">
+                      <FaFacebookF />
+                    </button>
+                    <button className="bg-[#f98d2f] text-white p-2 ">
+                      <FaTwitter />
+                    </button>
+                    <button className="bg-[#f98d2f] text-white p-2 ">
+                      <FaLinkedin />
+                    </button>
+                  </div>
+                </div>
+                <div className="w-[20rem] h-[5rem] flex flex-col text-center  justify-center bg-[#f98d2f] mt-0">
+                  <h5 className="mb-0 font-bold text-xl mt-6"> Mrs.S.Ramya</h5>
+                  <small className="font-bold text-sm">
+                    Acadamic Co-ordinator
+                  </small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
